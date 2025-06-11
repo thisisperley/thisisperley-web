825047a5928e753624c891ef2901f4683edff7fb
Tim Miller <tim@rocket5studios.com>
2025-06-11 16:12:01 -0400

    Added noise effect from reactbits.dev

--- src/components/layout/ClientThemeLayout.tsx
+++ src/components/layout/ClientThemeLayout.tsx
@@ -3,6 +3,7 @@
 import { ReactNode } from "react";
 import { ThemeProvider } from "@/context/ThemeContext";
 import BlobEffect from "@/components/ui/BlobEffect";
+import Noise from "@/components/ui/Noise";
 
 interface ClientThemeLayoutProps {
   children: ReactNode;
@@ -23,6 +24,7 @@ export const ClientThemeLayout = ({ children }: ClientThemeLayoutProps) => {
           blob3: 80
         }}
       />
+      <Noise patternAlpha={20} patternRefreshInterval={4} />
       <main className="flex-1">{children}</main>
     </ThemeProvider>
   );
