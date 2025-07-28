d7d8114cfc3b4f4f969bc8e8d2dc0a9efd90811c
Tim Miller <tim@rocket5studios.com>
2025-07-28 17:23:03 -0400

    Update manifest file to specify icon sizes and modify Footer component's SVG path for improved accessibility and consistency.

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,7 +14,7 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: "/favicon.ico",
-        sizes: "any",
+        sizes: "16x16 32x32",
         type: "image/x-icon",
       },
       {
